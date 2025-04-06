2ffdf34d0f2242928709189e7f279e4166e54759
TECHHUB <giri.educom007@gmail.com>
2025-04-06 15:59:48 +0530

    hey i am trying to shift my eclipse project

---
+++ JanFebDemoPro/src/org/techhub/PowerApp.java
@@ -0,0 +1,14 @@
+package org.techhub;
+
+public class PowerApp {
+
+	public static void main(String[] args) {
+		 int base=5,index=4;
+		 int p=1;
+		 for(int i=1; i<=index;i++) {
+			  p = p*base;
+		 }
+		 System.out.println("Power is "+p);
+	}
+
+}
---
+++ JanFebDemoPro/src/org/techhub/TableApp.java
@@ -0,0 +1,11 @@
+package org.techhub;
+
+public class TableApp {
+
+	public static void main(String[] args) {
+		 for(int i=1;i<=10;i++) {
+			 System.out.println(i*10);
+		 }
+	}
+
+}
